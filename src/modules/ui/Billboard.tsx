484f5a0e32be09064f2ff3e23996f6cf70427a6c
Marcelo Araújo Silva <marceloaraujosilva739@gmail.com>
2026-03-01 21:48:15 -0300

    minor changes

--- src/modules/ui/Billboard.tsx
+++ src/modules/ui/Billboard.tsx
@@ -5,7 +5,7 @@ export function PtBillboard () {
             <h1 className="text-white font-bold text-center text-4xl sm:text-6xl font-[var(--primary-font)]
             animate-(--typing-animation) whitespace-nowrap overflow-hidden">Marcelo A. Silva</h1>
             <h2 className="text-white font-normal text-center text-1xl sm:text-4xl font-[var(--primary-font)]
-            animate-(--typing-animation) whitespace-nowrap overflow-hidden">Ciência da Computação - Dev - Fullstack</h2>
+            animate-(--typing-animation) whitespace-nowrap overflow-hidden">Dev. Fullstack - Cientista de Dados</h2>
         </div>
     );
 }
@@ -17,7 +17,7 @@ export function EnBillboard () {
             <h1 className="text-white font-bold text-center text-4xl sm:text-6xl font-[var(--primary-font)]
             animate-(--typing-animation) whitespace-nowrap overflow-hidden">Marcelo A. Silva</h1>
             <h2 className="text-white font-normal text-center text-1xl sm:text-4xl font-[var(--primary-font)]
-            animate-(--typing-animation) whitespace-nowrap overflow-hidden">Computer Science - Dev - Fullstack</h2>
+            animate-(--typing-animation) whitespace-nowrap overflow-hidden"> Dev Fullstack - Data Scientist</h2>
         </div>
     );
 }
