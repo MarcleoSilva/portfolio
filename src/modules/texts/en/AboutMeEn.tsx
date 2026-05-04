8626d87a4330cad7c2e606023613fd2b0d384f3e
Marcelo Araújo Silva <marceloaraujosilva739@gmail.com>
2026-05-04 19:18:33 -0300

    update about me

--- src/modules/texts/en/AboutMeEn.tsx
+++ src/modules/texts/en/AboutMeEn.tsx
@@ -1,3 +1,3 @@
 export const aboutMeTextEn = `
-Full Stack Developer and Data Scientist with a solid academic background in Computational Biology. My current technical focus is on building scalable applications, leveraging Java (Spring Boot) for backend architectures and TypeScript (React) for modern interfaces. I am a curious, adaptable professional, passionate about the intersection of technology and science.
+Full Stack Developer and Data Scientist with a solid academic background in Computational Biology. My current technical focus is on building scalable applications, using JavaScript/TypeScript (NestJS) for backend architectures and TypeScript (React) for modern interfaces. I am a curious, adaptable professional, passionate about the intersection of technology and science. Currently, Trainee in Backend Development at TITAN.
 `;
--- src/modules/texts/pt/AboutMePt.tsx
+++ src/modules/texts/pt/AboutMePt.tsx
@@ -1,3 +1,3 @@
 export const aboutMeText = `
-Desenvolvedor Full Stack e Cientista de Dados com sólido background acadêmico em Biologia Computacional. Atualmente, foco meu desenvolvimento técnico no desenvolvimento de aplicações escaláveis, utilizando Java (Spring Boot) para arquiteturas de backend e TypeScript (React) para interfaces modernas. Sou curioso, adaptável e apaixonado pela tecnologia e ciência.
+Desenvolvedor Full Stack e Cientista de Dados com sólido background acadêmico em Biologia Computacional. Atualmente, foco meu desenvolvimento técnico no desenvolvimento de aplicações escaláveis, utilizando JavaScript/TypeScript (NestJS) para arquiteturas de backend e TypeScript (React) para interfaces modernas. Sou curioso, adaptável e apaixonado pela tecnologia e ciência. Atualmente, Trainee em Desenvolvimento Backend na empresa TITAN.
 `;
